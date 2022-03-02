7e224dd6efcf3dcc1646ef77ecc80f76ea0178ff
Graham Keeth <graham.keeth@gmail.com>
2022-03-01 20:39:44 -0500

    setup PD11 as a status GPIO

--- common/gpio.c
+++ common/gpio.c
@@ -9,4 +9,7 @@ void gpio_setup(void) {
     rcc_periph_clock_enable(RCC_GPIOD);
     gpio_mode_setup(PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED0 | PIN_LED1);
     // gpio_mode_setup(PORT_SWITCH, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, PIN_SWITCH);
+
+    // for toggling on the logic analyzer
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO11);
 }
