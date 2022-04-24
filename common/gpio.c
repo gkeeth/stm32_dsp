b80d895970ace0f38e2af88434d207c3ae188c21
Graham Keeth <graham.keeth@gmail.com>
2022-04-24 16:11:44 -0400

    configure GPIO input for button
    button on r0 board is miswired and relies on internal pullup in stm32.
    r1 board pulls up switch to 3.3V on pcb and can remove internal stm32
    pullup.

--- common/gpio.c
+++ common/gpio.c
@@ -8,8 +8,11 @@
 void gpio_setup(void) {
     rcc_periph_clock_enable(RCC_GPIOD);
     gpio_mode_setup(PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED0 | PIN_LED1);
-    // gpio_mode_setup(PORT_SWITCH, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, PIN_SWITCH);
 
     // for toggling on the logic analyzer
     gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO10 | GPIO11);
+
+    // for button input
+    // gpio_mode_setup(PORT_SWITCH, GPIO_MODE_INPUT, GPIO_PUPD_NONE, PIN_SWITCH)
+    gpio_mode_setup(PORT_SWITCH, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, PIN_SWITCH);
 }
